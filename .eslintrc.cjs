cd56a973c74717d518d40fe59ca54e8deb7f5bdc
Kyoung Hoon Kim <brian@groovejones.com>
2024-08-01 12:50:18 -0400

    basic ssl set up done and prettier set up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,21 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
